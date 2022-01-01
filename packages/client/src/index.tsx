776b99c10a5248d63ac401e9369116745f78ff93
MURHAF ELMASRI <48405071+MURHAF-ELMASRI@users.noreply.github.com>
2022-01-01 19:46:02 +0300

    create store file , add reac-redux

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { store } from "./state/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
