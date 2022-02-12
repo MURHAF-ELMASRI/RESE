af71e0c7d80b5e304b52f5f21dd5c72b3e0af9af
Recep alahmad <48405071+MURHAF-ELMASRI@users.noreply.github.com>
2022-02-12 22:35:26 +0300

    get server working with mongodb

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -5,9 +5,25 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { userRouter } from "./routes/userRouter";
 import { pitchRouter } from "./routes/pitchRouter";
+import mongo from "connect-mongo";
+import session from "express-session";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: ".env" });
+const MONGODB_URL = process.env.MONGODB_URL || "";
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("  MongoDB is connected successfully.");
+  })
+  .catch((err: any) => {
+    console.error(
+      "  MongoDB connection error. Please make sure MongoDB is running. " + err
+    );
+    process.exit();
+  });
 
 const app = express();
 
@@ -16,15 +32,15 @@ app.use(cookieParser());
 app.use(cors());
 
 //routes
+app.use("/", (req, res) => {
+  res.status(200).send("server is working");
+});
+
 app.use("/user", userRouter);
 app.use("/pitches", pitchRouter);
 
-const MONGODB_URL = process.env.MONGODB_URL;
-mongoose.connect(MONGODB_URL).catch((error) => {
-  console.log(error.message);
-});
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
