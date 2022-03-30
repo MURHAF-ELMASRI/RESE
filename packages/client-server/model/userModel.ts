a4c0fd32649e8b568abb33476dcdff9556cf07c6
MURHAF-ELMASRI <murhaf.msri@gmail.com>
2022-03-30 19:47:51 +0300

    change user type

--- packages/client-server/model/userModel.ts
+++ packages/client-server/model/userModel.ts
@@ -1,3 +1,10 @@
-
-
-export type userType = "normal"|"owner"
+export type UserTypeType = "normal" | "manger";
+export interface User {
+  _id: string;
+  fullName: string;
+  password: string;
+  salt: string;
+  phone: number;
+  email: string;
+  userType: UserTypeType;
+}
